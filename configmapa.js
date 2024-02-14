d3409606ecc1adad5a92e12e56ba18a1b47d5e72
LuisVitorVaz <105644153+LuisVitorVaz@users.noreply.github.com>
2024-02-14 13:25:59 -0300

    Add files via upload

--- configmapa.js
+++ configmapa.js
@@ -1,56 +1,102 @@
-// import {dataValor} from './index.js'
-// console.log("aqui no arquivo");
-// console.log(dataValor);
+var cont = 0;
+var dataValor;
+var horaValor;
+var lat;
+var long;
+var coordenadas_iniciais_lat, coordenadas_iniciais_lon;
+var coordenadas2,coordenadas3;
+var dadosList = document.getElementById("data-box");
+var database = firebase.database();
 
-let myCustomColourUser = 'background-color: red;';
-const markerHtml = `
-	width: 3rem;
-	height: 3rem;
-	display: block;
-	left: -1.5rem;
-	top: -1.5rem;
-	position: relative;
-	border-radius: 3rem;
-	transform: rotate(45deg);
-	border: 3px solid #FFFFFF;`;
+var dadosRef = database.ref("dados"); // Substitua pelo ID correto do seu nó de dados
 
-  const coordTaxi = [-30.0326, -51.2300]; // Porto Alegre, Brasil
+var map; // Variável para armazenar o mapa
 
-  // Coordenadas do Ponto B - Usuário
-  const coordUser = [-30.0277, -51.2287]; // Coordenadas fictícias do usuário
-  
+// Evento "child_added" para lidar com novos dados
+dadosRef.on("child_added", function(childSnapshot) {
+  // Acessa os valores diretamente no nível do childSnapshot
+  dataValor = childSnapshot.child("dado1").val();
+  horaValor = childSnapshot.child("dado2").val();
+  console.log("Novo dado adicionado:");
+  console.log(dataValor);
+  console.log(horaValor);
 
-// Iniciar o mapa com coordenadas do ponto A
-const map = L.map('map').setView(coordTaxi, 13);
+  // Exemplo de uso
+  const coordenadas1 = converterParaCoordenadas(dataValor, horaValor);
+  console.log("aqui esta \n");
+  console.log("Latitude:", coordenadas1.latitude);
+  console.log("Longitude:", coordenadas1.longitude);
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+  // recupera a lat e longitude inicialmente
+  if (cont == 0) {
+    coordenadas_iniciais_lat = coordenadas1.latitude;
+    coordenadas_iniciais_lon = coordenadas1.longitude;
+    console.log("dados iniciais");
+    cont = 50;
+  }
 
-// Personalizar Ponto no mapa com imagem do táxi.
-const taxiIcon = L.icon({
-  className: "taxi-pointers",
-  iconUrl: 'car-top-view.png',
-  iconSize: [45, 45]
+  // Verifica se o mapa já foi inicializado
+  if (!map) {
+    iniciarMapa(coordenadas_iniciais_lat, coordenadas_iniciais_lon,coordenadas1.latitude,coordenadas1.longitude);
+  }
+
+  // Atualiza o HTML com os novos valores
+  atualizarHTML(coordenadas1.latitude, coordenadas1.longitude);
 });
-const taxiMarker = L.marker(coordTaxi, { icon: taxiIcon }).addTo(map);
+// teste1 e teste2  contem os dados apos o primeiro dado postado no banco no caso e onde sera recebido lat e long apos o ponto incial
+function iniciarMapa(lat, lon,teste1,teste2) {
+  console.log("dados dentro do mapa :", teste1);
+  console.log("dados dentro do mapa : ", teste2);
+  // Coordenadas do Ponto A - Porto Alegre, Brasil
+  const coordTaxi = [lat, lon];
 
-// Identificar a melhor rota para iniciar a viagem.
-L.Routing.control({
-  waypoints: [
-    L.latLng(coordTaxi[0], coordTaxi[1]),
-    L.latLng(coordUser[0], coordUser[1])
-  ]
-}).on('routesfound', function (e) {
-  const latlng = e.routes[0].coordinates;
+  // Coordenadas do Ponto B - Usuário (coordenadas fictícias)
+  const coordUser = [teste1, teste2];
 
-  // Simular o táxi enviando a localização para o APP.
-  latlng.forEach(function (coord, index) {
-    setTimeout(function () {
-      taxiMarker.setLatLng([coord.lat, coord.lng]);
+  // Inicia o mapa com coordenadas do ponto A
+  map = L.map('map').setView(coordTaxi, 13);
 
-      // Identificar o final da viagem.
-      if (coord.lat === coordUser[0] && coord.lng === coordUser[1]) {
-        alert('Seu táxi acabou de chegar!');
-      }
-    }, 1000 * index);
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+
+  // Personaliza o ponto no mapa com imagem do táxi.
+  const taxiIcon = L.icon({
+    className: "taxi-pointers",
+    iconUrl: 'car-top-view.png',
+    iconSize: [45, 45]
   });
-}).addTo(map);
+  const taxiMarker = L.marker(coordTaxi, { icon: taxiIcon }).addTo(map);
+
+  // Identifica a melhor rota para iniciar a viagem.
+  L.Routing.control({
+    waypoints: [
+      L.latLng(coordTaxi[0], coordTaxi[1]),
+      L.latLng(coordUser[0], coordUser[1])
+    ]
+  }).on('routesfound', function(e) {
+    const latlng = e.routes[0].coordinates;
+
+    // Simula o táxi enviando a localização para o APP.
+    latlng.forEach(function(coord, index) {
+      setTimeout(function() {
+        taxiMarker.setLatLng([coord.lat, coord.lng]);
+
+        // Identifica o final da viagem.
+        if (coord.lat === coordUser[0] && coord.lng === coordUser[1]) {
+          alert('Seu táxi acabou de chegar!');
+        }
+      }, 1000 * index);
+    });
+  }).addTo(map);
+}
+
+function atualizarHTML(dataValor, horaValor) {
+  // Atualiza o HTML com os valores correspondentes
+  document.getElementById("latitude").textContent = dataValor;
+  document.getElementById("longitude").textContent = horaValor;
+}
+
+function converterParaCoordenadas(latitude, longitude) {
+  lat = latitude / 1e6; // Dividir por 1 milhão para obter a parte decimal correta
+  long = longitude / 1e6;
+  return { latitude: lat, longitude: long };
+}
